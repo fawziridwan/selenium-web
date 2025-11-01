421eb825a92bf291867fc3bd06d3061a7c630a40
fawziridwan <fawziridwan@gmail.com>
2025-11-01 13:55:09 +0700

    update scenario

--- src/test/java/com/automation/config/BaseTest.java
+++ src/test/java/com/automation/config/BaseTest.java
@@ -2,6 +2,9 @@ package com.automation.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -11,16 +14,33 @@ public class BaseTest {
     protected WebDriverWait wait;
 
     public void setUp() {
-        driver = new ChromeDriver();
+        String browser = ConfigReader.getBrowser();
+        boolean headless = Boolean.parseBoolean(ConfigReader.getHeadless());
+
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            case "chrome":
+            default:
+                ChromeOptions options = new ChromeOptions();
+                if (headless) {
+                    options.addArguments("--headless");
+                }
+                driver = new ChromeDriver(options);
+                break;
+        }
+
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(ConfigReader.getImplicitTimeout()));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(ConfigReader.getExplicitTimeout()));
     }
 
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        if (driver != null) driver.quit();
     }
 
     public WebDriver getDriver() {
